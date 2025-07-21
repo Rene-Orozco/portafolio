deca102cee63b3524b809a247afc1588ffb85dc9
rene-orozco <oroozco.rene@gmail.com>
2025-07-21 14:45:42 -0500

    muchas cosas

--- src/components/CartaContactos/CartaContactos.jsx
+++ src/components/CartaContactos/CartaContactos.jsx
@@ -19,18 +19,18 @@ function CartaContactos() {
         <>
             <div className={estilos.padre}>
                 {/* bloque de email */}
-                <div className={`${desicion ? estilos.mostrar : estilos.ocultar} ${estilos.acomodar}`}>
+                <div className={`${estilos.acomodar}`}>
                     <div>
                         <h2>email</h2>
                         <img src={email} alt="email" width={300} />
                         <p>
-                            usar <button onClick={cambiar}>
+                            Nuevo MSJ <button onClick={cambiar}>
                                 <img src={whatsapp} alt="whatsapp" width={20} />
                             </button>
                         </p>
                     </div>
 
-                    <div>
+                    <div className={`${desicion ? estilos.mostrar : estilos.ocultar}`}>
                         <form action="https://formsubmit.co/03dbaa6d2c4d82cd78ee533125df473c" method="POST" className={estilos.formulario}>
                             <label htmlFor="name">Nombre</label>
                             <input type="text" name="name" required />
@@ -43,27 +43,13 @@ function CartaContactos() {
 
                             <label htmlFor="mensaje">mensaje</label>
                             <textarea name="mensaje" id="mensaje"></textarea>
+
+                            <input type="hidden" name="_captcha" value="false"></input>
                             <button type="submit">Send</button>
                         </form>
                     </div>
                 </div>
 
-
-
-
-                    {/* bloque de whappsap */}
-                    <div className={`${desicion ? estilos.ocultar : estilos.mostrar}`}>
-                        <div>
-                            <h2>whatsapp</h2>
-                            <img src={whatsapp} alt="whatsapp" width={300} />
-                            <p>
-                                usar <button onClick={cambiar}>
-                                    <img src={email} alt="email" width={20} />
-                                </button>
-                            </p>
-                        </div>
-                        <div></div>
-                    </div>
                 </div>
             </>
             )
